01cc6bb870fa75c6a0312790ec485170435de236
Almaz <almich_rpk@list.ru>
2012-05-31 17:06:01 +0600

    firstCommit
    Signed-off-by: Almaz <almich_rpk@list.ru>

---
+++ webf/webf/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webf.Models;
+using webf.Models.EntityModel;
+
+namespace webf.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            
+            return View();
+        }
+        [HttpGet]
+        public ActionResult List()
+        {
+            
+            Days weekDay = new Days();
+            weekDay.CurrentDay = 5;
+
+            return View(weekDay);
+        }
+        [HttpPost]
+        public ActionResult List(FormCollection collection)
+        {
+
+            var value = collection["CurrentDay"];
+
+            return RedirectToAction("Index");
+        }
+        [Authorize(Roles = "Orbitr")]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult UserRegistrationBlank()
+        {
+            RegForm _blank = new RegForm();
+            ViewData.Model = _blank;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UserRegistrationBlank(RegForm obj)
+        {
+            RegForm _blank = new RegForm();
+            ViewData.Model = _blank;
+            return View();
+        }
+        /*public ActionResult KeyGen()
+        {
+            return View();
+        }*/
+    }
+}
---
+++ webf/webf/Models/UserProfile.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webf.App_GlobalResources;
+
+namespace webf.Models.EntityModel
+{
+    
+    public class RegForm//: DefaultModelBinder
+    {
+        public  RegForm()
+        {}
+
+        [Required]
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Имя")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Отчество")]
+        public string ParentName { get; set; }
+        
+        [Display(Name = "Дата рождения")]
+        [RegularExpression(@"\d{1,2}\.\d{1,2}\.\d{4}", 
+            ErrorMessageResourceName = "DateFormatIncorrect", 
+            ErrorMessageResourceType = typeof(ErrorStrings))]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        [Display(Name = "Должность")]
+        public string Post { get; set; }
+        
+        [Display(Name = "Телефон(домащний)")]
+        public string PhoneHomeNum { get; set; }
+
+        [Required]
+        [Display(Name = "Телефон(мобильный)")]
+        public string PhoneMobile { get; set; }
+
+        [Display(Name = "Телефон(ОС)")]
+        public string PhoneOS { get; set; }
+
+        [Display(Name = "Телефон(рабочий городской)")]
+        public string PhoneGTS { get; set; }
+
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        
+        public HttpPostedFileBase ImgFile { get; set; }
+
+        
+        public Guid LoginID { get; set; }
+        public int KeyRingId { get; set; }
+    }
+
+    
+}
